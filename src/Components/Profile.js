1a7c5a8edbc53ab6f8fa9233a4fd43d2d54b11b9
Hugo Tran <006.tran.h@gmail.com>
2022-06-04 20:00:07 -0700

    Updating profile component
    Adding data from the github api call to react card component

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, ListGroup } from "react-bootstrap";
 
 function Profile() {
   const handle = "https://api.github.com/users/howtoduck";
@@ -14,17 +14,33 @@ function Profile() {
   if (!Gitinfo) return "";
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className="profile justify-content-center align-items-center d-flex bg-light">
+      <Card className="bg-light" style={{ width: "18rem" }}>
+        <Card.Header>Github User</Card.Header>
+        <Card.Img variant="top" src={Gitinfo.avatar_url} />
+        <Card.Body>
+          <Card.Title class="card text-center bg-light p-2">
+            {Gitinfo.login}
+          </Card.Title>
+          <Card.Text>
+            <ListGroup variant="flush">
+              <ListGroup.Item className="bg-light">
+                {Gitinfo.name}
+              </ListGroup.Item>
+              <ListGroup.Item className="bg-light">
+                {Gitinfo.location}
+              </ListGroup.Item>
+            </ListGroup>
+            {Gitinfo.bio}
+          </Card.Text>
+          <Button size="sm">Website</Button>
+          <Button size="sm">Github</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">
+          Public Repositories: {Gitinfo.public_repos}
+        </Card.Footer>
+      </Card>
+    </div>
   );
 }
 
